ff8853302f45cab83b257a3d6b049480e8caa796
Saint Wesonga <saint.wesonga@microsoft.com>
2024-09-23 09:40:05 -0600

    Add separate Universe::is_in_heap assertion for easy breakpoint setting

--- src/hotspot/share/oops/compressedOops.inline.hpp
+++ src/hotspot/share/oops/compressedOops.inline.hpp
@@ -55,6 +55,11 @@ inline oop CompressedOops::decode_not_null(narrowOop v) {
   assert(!is_null(v), "narrow oop value can never be zero");
   oop result = decode_raw(v);
   assert(is_object_aligned(result), "address not aligned: " PTR_FORMAT, p2i(result));
+  bool is_in_heap = Universe::is_in_heap(result);
+
+  if (!is_in_heap) {
+    assert(false, "object not in heap " PTR_FORMAT, p2i(result));
+  }
   assert(Universe::is_in_heap(result), "object not in heap " PTR_FORMAT, p2i(result));
   return result;
 }
