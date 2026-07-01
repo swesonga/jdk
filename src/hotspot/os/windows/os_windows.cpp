4c1f150b14649aedd108aacd592e9432b2128967
Saint Wesonga <saint.wesonga@microsoft.com>
2026-06-30 22:50:34 -0600

    Do not terminate the JVM on EXCEPTION_GUARD_PAGE

--- src/hotspot/os/windows/os_windows.cpp
+++ src/hotspot/os/windows/os_windows.cpp
@@ -2869,7 +2869,8 @@ LONG WINAPI topLevelExceptionFilter(struct _EXCEPTION_POINTERS* exceptionInfo) {
 #if defined(_M_ARM64)
   should_report_error = should_report_error &&
                         FAILED(exception_code) &&
-                        (exception_code != EXCEPTION_UNCAUGHT_CXX_EXCEPTION);
+                        (exception_code != EXCEPTION_UNCAUGHT_CXX_EXCEPTION) &&
+                        (exception_code != EXCEPTION_GUARD_PAGE);
 #endif
 
   if (should_report_error) {
